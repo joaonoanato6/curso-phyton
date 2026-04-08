2c2507d899d551b6e5004ca7b4e12e7984eb5c67
joaonoanato6 <joaononato428@gmail.com>
2026-04-08 20:05:27 -0300

    Create exercio4.py

---
+++ exercio4.py
@@ -0,0 +1,8 @@
+#faça um algoritmo que peça um numero e informe se ele é maior que 10 
+print("fala um numero")
+numero = float(input())
+if numero >10:
+    print("numero e maior que 10")
+else:
+    print("numero e menor que 10")
+    
