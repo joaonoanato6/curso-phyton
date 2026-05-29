db15227e306037dc1744c8ea258a103fb5a49427
joaonoanato6 <joaononato428@gmail.com>
2026-05-29 18:11:24 -0300

    Nuvem, estudo.py

---
+++ Nuvem, estudo.py
@@ -0,0 +1,19 @@
+ iaas = infraestrutura como serviço.
+ definição = IaaS (Infraestrutura como Serviço) é um modelo de computação em nuvem onde provedores 
+ terce tópicos sobre computação em nuvem:
+irizados fornecem recursos fundamentais de TI — como servidores virtuais, armazenamento e redes
+ sob demanda e via internet. A principal vantagem é o pagamento conforme
+ o uso, o que elimina a necessidade de comprar e manter data centers físicos.
+   (data centers) um lugar, infraestrutura bem organizada para armazenar processos envolvendo trabalho
+ de grande escala,e de continuo. podendo ser em locais físicos ou em nuvem.
+
+ A espinha dorsal de um data center é a redundância.
+ Para garantir que serviços online, aplicativos e sites funcionem 24 horas por dia, 365 dias por ano,
+ essas instalações possuem.
+
+    Principais modos de uso.
+ Corporativos: Estruturas próprias, montadas exclusivamente para atender as necessidades de uma única grande empresa.
+
+   (SGBD) significa Sistema Gerenciador de Banco de Dados. É o software responsável por criar, organizar,
+ manipular e proteger bancos de dados. Ele funciona como uma ponte segura entre a sua aplicação e os dados armazenados,  
+garantindo a integridade e a consistência das informações.
